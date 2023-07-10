2e1ca311a0243a4089324d3e8e9cd3e86ca9f481
Kingsley Asifor <kingsleyasifor@gmail.com>
2023-07-10 13:27:41 +0100

    update BarChart

--- components/BarCharts.jsx
+++ components/BarCharts.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Bar } from 'react-chartjs-2';
 import {
     Chart as ChartJS,
@@ -10,11 +10,29 @@ import {
     Legend,
 } from 'chart.js';
 
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
 
 const BarCharts = () => {
-  return (
-    <div>BarCharts</div>
-  )
+    const [chartData, setChartData] = useState({
+        datasets: [],
+    });
+
+    const [chartOptions, setChartOptions] = useState({});
+
+    return (
+        <>
+            <div className='w-full md:col-span-2 relative lg:h-[700] h-[50vh] m-auto p-4 border rounded-lg bg-white'>
+                <Bar data={chartData} options={chartOptions} />
+            </div>
+        </>
+    )
 }
 
 export default BarCharts
