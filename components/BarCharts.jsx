9268ad83fc830494e79c40d7b90d2c892e47a571
Kingsley Asifor <kingsleyasifor@gmail.com>
2023-07-10 13:04:14 +0100

    update BarChart

--- components/BarCharts.jsx
+++ components/BarCharts.jsx
@@ -1,4 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+
 
 const BarCharts = () => {
   return (
