47698454d4ee9aa3aef6a6b270752151b07c2a69
Kingsley Asifor <kingsleyasifor@gmail.com>
2023-07-10 22:18:51 +0100

    update orders.jsx

--- pages/orders.jsx
+++ pages/orders.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { data } from '@/data/data';
+import { FaShoppingBag } from 'react-icons/fa';
+import { BsThreeDotsVertical } from 'react-icons/bs';
 
 const orders = () => {
   return (
