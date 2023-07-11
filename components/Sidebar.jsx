c24ac74541ecf1e3679803f3206c4a6422037782
Kingsley Asifor <kingsleyasifor@gmail.com>
2023-07-11 18:49:02 +0100

    update customers.jsx, add head tag

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -57,7 +57,7 @@ const Sidebar = ({ children }) => {
                     {/* Sidebar Link */}
 
                     {/* Sidebar Link */}
-                    <Link href='/'>
+                    <Link href=''>
                         <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
                             <FiSettings size={20} />
                         </div>
--- pages/customers.jsx
+++ pages/customers.jsx
@@ -1,45 +1,54 @@
-import React from 'react'
-import { BsPersonFill, BsThreeDots, BsThreeDotsVertical } from 'react-icons/bs'
-import { data } from '../data/data.js'
+import React from 'react';
+import { BsPersonFill, BsThreeDots, BsThreeDotsVertical } from 'react-icons/bs';
+import { data } from '../data/data.js';
+import Head from 'next/head.js';
 
 const customers = () => {
-  return (
-    <div className='bg-gray-100 min-h-screen'>
-        <div className='flex justify-between p-4'>
-            <h2>Customers</h2>
-            <h2>Hola, Kings</h2>
-        </div>
-
-        <div className='p-4'>
-            <div className='w-full m-auto p-4 border rounded-lg bg-white overflow-y-auto'>
-                <div className='my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer'>
-                    <span>Name</span>
-                    <span className='sm:text-left text-right'>Email</span>
-                    <span className='hidden md:grid'>Last Order</span>
-                    <span className='hidden md:grid'>Method</span>
+    return (
+        <>
+            <Head>
+                <title>Customers List</title>
+                <meta name="description" content="Customers List" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className='bg-gray-100 min-h-screen'>
+                <div className='flex justify-between p-4'>
+                    <h2>Customers</h2>
+                    <h2>Hola, Kings</h2>
                 </div>
-                <ul>
-                    {data.map((order, id) => (
-                        <li key={id} className='bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer'>
-                            <div className='flex items-center'>
-                                <div className='bg-slate-100 p-3 rounded-lg'>
-                                    <BsPersonFill className='text-slate-800' />
-                                </div>
-                                <p className='pl-4'>{order.name.first + ' ' + order.name.last}</p>
-                            </div>
-                            <p className='text-gray-600 sm:text-left text-right'>{order.name.first}@gmail.com</p>
-                            <p className='hidden md:flex'>{order.date}</p>
-                            <div className='sm:flex hidden justify-between items-center'>
-                                <p>{order.method}</p>
-                                <BsThreeDotsVertical />
-                            </div>
-                        </li>
-                    ))}
-                </ul>
+
+                <div className='p-4'>
+                    <div className='w-full m-auto p-4 border rounded-lg bg-white overflow-y-auto'>
+                        <div className='my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer'>
+                            <span>Name</span>
+                            <span className='sm:text-left text-right'>Email</span>
+                            <span className='hidden md:grid'>Last Order</span>
+                            <span className='hidden md:grid'>Method</span>
+                        </div>
+                        <ul>
+                            {data.map((order, id) => (
+                                <li key={id} className='bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer'>
+                                    <div className='flex items-center'>
+                                        <div className='bg-slate-100 p-3 rounded-lg'>
+                                            <BsPersonFill className='text-slate-800' />
+                                        </div>
+                                        <p className='pl-4'>{order.name.first + ' ' + order.name.last}</p>
+                                    </div>
+                                    <p className='text-gray-600 sm:text-left text-right'>{order.name.first}@gmail.com</p>
+                                    <p className='hidden md:flex'>{order.date}</p>
+                                    <div className='sm:flex hidden justify-between items-center'>
+                                        <p>{order.method}</p>
+                                        <BsThreeDotsVertical />
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-  );
+        </>
+    );
 };
 
 export default customers
